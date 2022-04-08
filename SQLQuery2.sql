485bc09009c1e790f5a8eb7a1433422ad5ae0219
anphuongvu <vuphuongan2012@gmail.com>
2022-04-08 09:26:09 +0700

    push 1

---
+++ SQLQuery2.sql
@@ -0,0 +1,29 @@
+﻿CREATE DATABASE CSDL1
+USE CSDL1
+GO
+CREATE TABLE [Product]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [pName] NVARCHAR(150) NULL, 
+    [pType] NVARCHAR(150) NULL, 
+    [pPrice] FLOAT NULL, 
+    [categoryId ] INT NULL , 
+    [imgProduct] VARCHAR(150) NULL,
+    CONSTRAINT fk_idCategory 
+    FOREIGN KEY (categoryId )
+    REFERENCES Category (categoryId )
+
+);
+
+CREATE TABLE [Category]
+(
+    [categoryId] INT NOT NULL PRIMARY KEY, 
+    [categoryName] NVARCHAR(150) NULL
+
+);
+
+insert into Category values (1, 'abc');
+insert into Category values (2, 'def');
+
+insert into Product values (1, '123', 'abc', 1234, 1, '');
+insert into Product values (2, '321', 'abc', 1234, 1, '');
---
+++ SQLQuery3.sql
@@ -0,0 +1,25 @@
+﻿Create database CSDL
+Use CSDL
+Go
+CREATE TABLE [Product]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [pName] NVARCHAR(150) NULL, 
+    [pType] NVARCHAR(150) NULL, 
+    [pPrice] FLOAT NULL, 
+    [categoryId ] INT NULL , 
+    [imgProduct] VARCHAR(150) NULL,
+    CONSTRAINT fk_idCategory 
+    FOREIGN KEY (categoryId )
+    REFERENCES Category (categoryId )
+
+);
+
+CREATE TABLE [Category]
+(
+    [categoryId] INT NOT NULL PRIMARY KEY, 
+    [categoryName] NVARCHAR(150) NULL
+
+);
+
+
